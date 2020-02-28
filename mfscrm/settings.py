0daf48afa2ce15eb25e3632b047505318230ec7b
ktourek <59860579+ktourek@users.noreply.github.com>
2020-02-28 17:59:31 -0600

    updated things

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -121,9 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
