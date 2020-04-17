4cd912c10cf39016e16c6cc8d793065f41b170dd
ktourek <konnertourek@unomaha.edu>
2020-04-16 21:43:39 -0500

    Adding Part 2

--- crm/urls.py
+++ crm/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path, re_path
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 app_name = 'crm'
 urlpatterns = [
@@ -13,4 +15,8 @@ urlpatterns = [
     path('service/create/', views.service_new, name='service_new'),
     path('service/<int:pk>/edit/', views.service_edit, name='service_edit'),
     path('customer/<int:pk>/summary/', views.summary, name='summary'),     
+    url(r'^customers_json/', views.CustomerList.as_view()),
+
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
